d4f3f3f518e4a8c9749c2a8e72a93f7217a92b2f
Abd El-Rahman Mostafa <abdelrahman.abdeltawab03@eng-st.cu.edu.eg>
2025-03-26 10:53:45 +0200

    Ignoring the unneeded build options

--- build/vendor/glfw/cmake_install.cmake
+++ build/vendor/glfw/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/amostafa/CMP/OpenGL
+# Install script for directory: /home/amostafa/CMP/OpenGL/vendor/glfw
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -43,18 +43,7 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for each subdirectory.
-  include("/home/amostafa/CMP/OpenGL/build/vendor/glfw/cmake_install.cmake")
-
+  # Include the install script for the subdirectory.
+  include("/home/amostafa/CMP/OpenGL/build/vendor/glfw/src/cmake_install.cmake")
 endif()
 
-if(CMAKE_INSTALL_COMPONENT)
-  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
-else()
-  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
-endif()
-
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/amostafa/CMP/OpenGL/build/${CMAKE_INSTALL_MANIFEST}"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
